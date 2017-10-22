e617860d5eb10ef6e6aedd1f2287656815887ad9
forsithias <reeda@utschools.ca>
2017-10-21 20:47:08 -0400

    Update gothgf.js

--- gothgf.js
+++ gothgf.js
@@ -12,25 +12,25 @@ var bot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
-bot.on('ready', function (evt) {
+gothgf.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info(gothgf.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
+gothgf.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+    // It will listen for messages that will start with `~`
+    if (message.substring(0, 1) == '~') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
         args = args.splice(1);
         switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
+            // ~hello
+            case 'hello':
+                gothgf.sendMessage({
                     to: channelID,
-                    message: 'Pong!'
+                    message: 'The pain of life is unbearable.'
                 });
             break;
             // Just add any case commands if you want to..
